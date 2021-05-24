d10cab587b84532a3e1eaed5fde63fc6701bc33e
Am0rfu5 <1178902+Am0rfu5@users.noreply.github.com>
2021-05-23 21:21:36 -0600

    finished StakePool

--- contracts/StakePool.sol
+++ contracts/StakePool.sol
@@ -1,8 +1,14 @@
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
+
 
 import "./HEXProxy.sol";
 import "./StakeCalculator.sol";
 
+/**
+ * @title GovernedStakePool
+ * @dev GovernedStakePool is a simple staking pool that allows users to stake HEX for a fixed period of time.
+ */
 contract GovernedStakePool {
     uint256 internal constant STAKE_LENGTH = 7;
     uint256 internal constant MINIMUM_STAKED_HEX = 1000;
@@ -17,6 +23,10 @@ struct Stake {
     uint16 unlockedDay;
 }
 
+/**
+ * @title StakePool
+ * @dev StakePool is a simple staking pool that allows users to stake HEX for a fixed period of time.
+ */
 contract StakePool is GovernedStakePool, StakeCalculator {
     HEXProxy private _hex;
 
@@ -28,7 +38,10 @@ contract StakePool is GovernedStakePool, StakeCalculator {
     constructor(address hex_address) {
         _hex = HEXProxy(hex_address);
     }
-
+    
+    /**
+     * @dev startStake allows a user to stake HEX for a fixed period of time.
+     */
     function startStake() external {
         // determine the staking size and verify it.
         uint256 _stakedHearts = _hex.balanceOf(address(this));
@@ -57,7 +70,11 @@ contract StakePool is GovernedStakePool, StakeCalculator {
             )
         );
     }
-
+    
+    /**
+     * @dev endStake allows a user to end a stake before it's due.
+     * @param index The index of the stake to end.
+     */
     function endStake(uint256 index) external {
         // grab the relevant days
         uint256 currentDay = _hex.currentDay();
@@ -85,22 +102,39 @@ contract StakePool is GovernedStakePool, StakeCalculator {
         // TODO: transfer HEX back to shares contract'
     }
 
+    /**
+     * @dev lastStakeId returns the last stakeId.
+     */
     function lastStakeId() public view returns (uint256) {
         return _hex.globalInfo()[6];
     }
-
+    
+    /**
+     * @dev shareRate returns the current share rate.
+     */
     function shareRate() public view returns (uint256) {
         return _hex.globalInfo()[2];
     }
 
+    /**
+     * @dev currentStakeIndex returns the current stake index.
+     */
     function currentStakeIndex() public view returns (uint256) {
         return stakes.length;
     }
 
+    /**
+     * @dev stakeId returns the stakeId for a given stake index.
+     * @param stakeIndex The index of the stake.
+     */
     function stakeShares(uint256 stakeIndex) public view returns (uint72) {
         return stakes[stakeIndex].stakeShares;
     }
 
+    /**
+     * @dev stakeId returns the stakeId for a given stake index.
+     * @param stakeIndex The index of the stake.
+     */
     function stakedHearts(uint256 stakeIndex) public view returns (uint72) {
         return stakes[stakeIndex].stakedHearts;
     }
